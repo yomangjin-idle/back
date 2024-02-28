75c03020a56c9b7aa4c9563ffa501a07b8a46f07
KodaHye <kodhdh09@gmail.com>
2024-02-29 08:21:42 +0900

    fix : controller 수정

--- src/main/java/com/example/backend/controller/TourController.java
+++ src/main/java/com/example/backend/controller/TourController.java
@@ -12,38 +12,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.Date;
 import java.util.List;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/tour")
+@RequestMapping("/api")
 public class TourController {
 
     private final TourService tourService;
-
     @GetMapping("/test")
+    public ResponseEntity<Void> testController() {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/tour/test")
     public ResponseEntity<String> test() {
         return ResponseEntity.status(HttpStatus.OK).body("되라!!");
     }
 
-    @GetMapping("")
+    @GetMapping("/tour")
     public ResponseEntity<List<TourListResponse>> getTourList() {
         return ResponseEntity.status(HttpStatus.OK).body(tourService.getTourList());
     }
 
-    @GetMapping("/{tourId}")
+    @GetMapping("/tour/{tourId}")
     public ResponseEntity<TourDetailResponse> getTourDetail(@PathVariable int tourId) {
         return ResponseEntity.status(HttpStatus.OK).body(tourService.getTourDetail(tourId));
     }
 
-    @GetMapping("/{tourId}/speak")
+    @GetMapping("/tour{tourId}/speak")
     public ResponseEntity<TourSpeakResponse> getTourSpeak(@PathVariable int tourId) {
         return ResponseEntity.status(HttpStatus.OK).body(tourService.getTourSpeak(tourId));
     }
